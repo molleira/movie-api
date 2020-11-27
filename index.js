0ac25eb3c6434c115c9fffb5e12b71b7348b6f17
Marc Oller <marc.om@icloud.com>
2020-11-27 13:51:19 +0100

    Create Express endpoint routes

--- index.js
+++ index.js
@@ -49,28 +49,68 @@ let topMovies = [
     }
   ];
   
-  // use morgan to log requests
-  app.use(morgan('common'));
+// use morgan to log requests
+app.use(morgan('common'));
 
-  // GET requests
-  app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
-  });
+// welcome request
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
 
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
+// get all movies request
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
 
-  // serve documentation
-  app.use(express.static('public'));
+// get single movie request
+app.get('/movies/title', (req, res) => {
+res.json(topMovies.title);
+});
 
-  // error-handling middleware function
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-  
-  // listen for requests
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+// get genre request
+app.get('/movies/genres/title', (req, res) => {
+  res.json(topGenres.title);
+ });
+
+// get director request
+app.get('/movies/directors/name', (req, res) => {
+  res.json(topDirectors.name);
+});
+
+// post new user request
+app.post('/users', (req, res) => {
+  res.json(userData);
+});
+
+// put user update request
+app.put('/users/username', (req, res) => {
+  res.json(userData.username);
+});
+
+// post favorite request
+app.post('/users/username/movies/title', (req, res) => {
+  res.json(userData.favorite);
+});
+
+// delete favorite request
+app.delete('/users/username/movies/title', (req, res) => {
+  res.send('The movie was removed from your list');
+});
+
+// deletes user request
+app.delete('/users/username', (req, res) => {
+  res.send('Your account was deleted');
+
+// serve documentation
+app.use(express.static('public'));
+
+// error-handling middleware function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
