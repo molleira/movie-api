5b3a99ace371718a6c186e641996df250e91b192
Marc Oller <marc.om@icloud.com>
2020-12-12 11:03:10 +0100

    Define Local and JWT strategies

---
+++ passport.js
@@ -0,0 +1,44 @@
+// requires passport, jwt and models
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+// sets users model and strategies as variables
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+// defines http authentication for login requests
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+
+// defines jwt authentication
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+        .then ((user) => {
+            return callback(null, user);
+        })
+        .catch((error) => {
+            return callback(error)
+        });
+}));
