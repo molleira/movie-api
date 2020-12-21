d915c12eb738e1ec18a8006c9fc8ee96b020df18
Marc Oller <marc.om@icloud.com>
2020-12-21 16:51:45 +0100

    Fix Config Vars

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const Users = Models.User;
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }); */
-mongoose.connect('process.env.CONNECTION_URI', {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
