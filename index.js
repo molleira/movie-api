bb29978fca00e36b6a0116a7c90aa34f368af1dc
Marc Oller <marc.om@icloud.com>
2021-02-08 16:40:30 +0100

    Add get profile endpoint

--- index.js
+++ index.js
@@ -138,6 +138,18 @@ app.post('/users', [
     });
 });
 
+// allow to get user data
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // allow users to update their info
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
   check('Username', 'Username is required').isLength({ min: 5 }),
